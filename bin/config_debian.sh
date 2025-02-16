b8fde57f72c21c4f352125006e04515d1bac36bd
Aaron Kunde <aaron.kunde@adesso.de>
2025-02-16 16:51:02 +0100

    Add non-free packages to apt

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -2,3 +2,7 @@
 set -e
 
 echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf.d/10get
+sed /etc/apt/sources.list -e \
+    's/\(deb.* \)main/\1non-free non-free-firmware/' \
+    | tee /etc/apt/sources.list.d/debian.non-free.list
+apt-get update
