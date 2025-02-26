bfa64ad1efaabfdc9909bfff81859ee420627184
Aaron Kunde <aaron.kunde@adesso.de>
2025-02-26 07:05:19 +0100

    Add SSH agent configuration

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -2,3 +2,6 @@
 set -e
 
 echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
+echo '' >> ~/.profile
+cat bin/start_ssh-agent.sh >> ~/.profile
+echo 'AddKeysToAgent yes' > ~/.ssh/config
---
+++ bin/start_ssh-agent.sh
@@ -0,0 +1,8 @@
+SSH_AGENT_ENV_FILE=.ssh/agent-env
+
+if [ -z "$(pidof ssh-agent)" ]; then
+    ssh-agent | head -n2 > $SSH_AGENT_ENV_FILE
+fi
+if [ -f $SSH_AGENT_ENV_FILE ]; then
+    eval $(cat $SSH_AGENT_ENV_FILE)
+fi
