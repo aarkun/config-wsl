532de671328ea1c69eb560418d9d05dc37be8564
Aaron Kunde <aaron.kunde@adesso.de>
2025-02-26 11:24:38 +0100

    Add installation of current asdf versions

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -2,9 +2,17 @@
 set -e
 
 echo -e '\nexport GPG_TTY=$(tty)' >> ~/.profile
+
 echo '' >> ~/.profile
 cat bin/start_ssh-agent.sh >> ~/.profile
 echo 'AddKeysToAgent yes' > ~/.ssh/config
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
-echo -e '\nsource "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
-echo 'source "$HOME/.asdf/completions/asdf.bash" ' >> ~/.bashrc
+
+ASDF_VERSION=v0.16.4
+ASDF_INSTALLATION_FILE=asdf-$ASDF_VERSION-linux-amd64.tar.gz
+curl -L -O --output-dir /tmp \
+     "https://github.com/asdf-vm/asdf/releases/download/$ASDF_VERSION/$ASDF_INSTALLATION_FILE{,.md5}"
+grep $(md5sum /tmp/$ASDF_INSTALLATION_FILE | cut -d ' ' -f 1) /tmp/$ASDF_INSTALLATION_FILE.md5
+mkdir -p ~/.local/bin
+tar -C ~/.local/bin -x -f /tmp/$ASDF_INSTALLATION_FILE
+echo -e '\nPATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"' >> ~/.profile
+echo -e '\n. <(asdf completion bash)' >> ~/.bashrc
