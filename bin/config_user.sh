fee1e07423411b9351b474d90e742fb377e8edbf
Aaron Kunde <aaron.kunde@adesso.de>
2025-02-26 08:34:13 +0100

    Add installation of asdf

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -5,3 +5,6 @@ echo -e '\nexport GPG_TTY=$(tty)' >> ~/.profile
 echo '' >> ~/.profile
 cat bin/start_ssh-agent.sh >> ~/.profile
 echo 'AddKeysToAgent yes' > ~/.ssh/config
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+echo -e '\nsource "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo 'source "$HOME/.asdf/completions/asdf.bash" ' >> ~/.bashrc
