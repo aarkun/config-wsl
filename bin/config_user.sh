0c663d73b5ff00644e23cf7e836994fa26497dfb
Aaron Kunde <aaron.kunde@adesso.de>
2025-02-27 15:06:26 +0100

    Add installation of Java

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -46,6 +46,10 @@ asdf set -u azure-cli latest
 asdf plugin add golang
 asdf install golang latest
 
+asdf plugin add java
+asdf install temurin-21.0.5+11.0.LTS
+asdf set -u temurin-21.0.5+11.0.LTS
+
 IDEA_INSTALLATION_FILE=ideaIU-2024.3.3.tar.gz
 curl -O --output-dir /tmp \
      "https://download-cdn.jetbrains.com/idea/$IDEA_INSTALLATION_FILE{,.sha256}"
