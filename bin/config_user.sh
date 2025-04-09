21b079d9e53ef00fc7f09b7cceff146b69fcff19
Aaron Kunde <aaron.kunde@adesso.de>
2025-04-09 13:25:10 +0200

    Remove temporary script file

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -13,8 +13,17 @@ PATH="$HOME/.local/bin:$PATH"
 
 echo '\nexport GPG_TTY=$(tty)' >> ~/.profile
 
-echo '' >> ~/.profile
-cat $(dirname $_)/start_ssh-agent.sh >> ~/.profile
+cat <<'EOF' >> ~/.profile
+
+SSH_AGENT_ENV_FILE=~/.ssh/agent-env
+
+if [ -z "$(pidof ssh-agent)" ]; then
+    ssh-agent | head -n2 > $SSH_AGENT_ENV_FILE
+fi
+if [ -f $SSH_AGENT_ENV_FILE ]; then
+    eval $(cat $SSH_AGENT_ENV_FILE)
+fi
+EOF
 mkdir -p ~/.ssh
 echo 'AddKeysToAgent yes' > ~/.ssh/config
 
