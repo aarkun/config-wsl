b97e4c7b241b613a92f97cdda2a6f5df62462df3
Aaron Kunde <aaron.kunde@adesso.de>
2025-04-25 15:55:58 +0200

    Reworked Python installation

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -69,9 +69,8 @@ asdf install make latest
 asdf set -u make latest
 
 asdf plugin add python
-asdf install python 3.11.11
-asdf set -u python 3.11.11
 
+asdf set -u python 3.10.11
 asdf plugin add azure-cli
 asdf install azure-cli latest
 asdf set -u azure-cli latest
