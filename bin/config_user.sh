bdbe40649f443043c16b5540d3f0102b788aee03
Aaron Kunde <aaron.kunde@adesso.de>
2025-04-25 11:20:25 +0200

    Add Node.js dependency for IntelliJ

--- bin/config_user.sh
+++ bin/config_user.sh
@@ -99,6 +99,8 @@ mkdir -p ~/opt
 tar -C ~/opt -x -f /tmp/$IDEA_INSTALLATION_FILE
 mkdir -p ~/opt
 find opt/ -name idea -exec ln -s ~/{} ~/.local/bin/idea \;
+asdf install nodejs 18.7.0
+asdf set -u nodejs 18.7.0
 
 DBEAVER_VERSION=25.0.0
 DBEAVER_INSTALLATION_FILE=dbeaver-ce-$DBEAVER_VERSION-linux.gtk.x86_64.tar.gz
