87299c46f1434ae0c3e664b2980860919b7c69c4
Aaron Kunde <aaron.kunde@adesso.de>
2025-06-30 10:50:14 +0200

    Add installation of NVIDIA container toolkit

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -60,6 +60,16 @@ apt-get install -y --mark-auto \
 	aardvark-dns
 apt-get install -y podman-compose
 
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+    | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+    | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+apt-get install -y nvidia-container-toolkit
+apt-get update
+apt-get install -y nvidia-container-toolkit
+nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
+
 apt-get install -y \
 	gcc \
 	zlib1g-dev \
