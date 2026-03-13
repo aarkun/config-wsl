0b0e1967499a90850d930a3c19a805dfccb54ec1
Aaron Kunde <aaron.kunde@adesso.de>
2026-03-13 16:17:26 +0100

    FIX: Remove too early installation of nvidia-container-toolkit

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -66,7 +66,6 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
     | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
     | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-apt-get install -y nvidia-container-toolkit
 apt-get update
 apt-get install -y nvidia-container-toolkit
 nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
