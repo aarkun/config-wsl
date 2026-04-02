fb6155c69456cdbb26bf0919a769754f159f2532
Aaron Kunde <aaron.kunde@adesso.de>
2026-04-02 15:32:58 +0200

    Moved apt-update into previous function

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -5,11 +5,10 @@ echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf.d/10get
 sed /etc/apt/sources.list -e \
     's/\(deb.* \)main/\1non-free non-free-firmware/' \
     | tee /etc/apt/sources.list.d/debian.non-free.list
+apt-get update
 update-alternatives --set editor /usr/bin/vim.tiny
 sed -iorig -e 's/# de_DE\.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
-
-apt-get update
 
 apt-get install -y dbus-user-session
 
