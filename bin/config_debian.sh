c2abd6e3ba704a5a74a30309e9e3fb1b0e9e7919
Aaron Kunde <aaron.kunde@adesso.de>
2026-04-03 17:53:49 +0200

    Do not use tee since it is not necessary

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -4,7 +4,7 @@ set -e
 echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf.d/10get
 sed /etc/apt/sources.list -e \
     's/\(deb.* \)main/\1non-free non-free-firmware/' \
-    | tee /etc/apt/sources.list.d/debian.non-free.list
+    > /etc/apt/sources.list.d/debian.non-free.list
 apt-get update
 update-alternatives --set editor /usr/bin/vim.tiny
 sed -i.orig -e 's/# de_DE\.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen
