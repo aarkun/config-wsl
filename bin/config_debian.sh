93f376919e048e75cfb2f51f777dac36387793d7
Aaron Kunde <aaron.kunde@adesso.de>
2026-08-17 10:45:43 +0200

    FIX: Prevent gpg from asking to override files

--- bin/config_debian.sh
+++ bin/config_debian.sh
@@ -39,7 +39,8 @@ apt-get install -y podman-compose
 
 apt-get install -y --mark-auto curl
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
-    | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+    | gpg --yes --dearmor \
+	  -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
     | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
     > /etc/apt/sources.list.d/nvidia-container-toolkit.list
